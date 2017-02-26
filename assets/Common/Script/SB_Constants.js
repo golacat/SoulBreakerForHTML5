19a48713252259e7d4b25aae3392ed25d7740d41
golacat <jongchoel@gmail.com>
2017-02-26 17:18:32 +0900

    add SB_Constants.js

---
+++ assets/Common/Script/SB_Constants.js
@@ -0,0 +1,17 @@
+var SB_Constants = cc.Class({
+    extends: cc.Component,
+    properties: {
+
+    },
+    statics: {
+        MAIN_WIDTH: 480,
+        MAIN_HEIGHT: 800,
+
+        TITLE_CIRCLE_YELLOW: cc.color(255, 251, 118),
+        TITLE_CIRCLE_BLUE: cc.color(166, 255, 253),
+        TITLE_CIRCLE_VIOLET: cc.color(244, 140, 203),
+        TITLE_TEXT_COLOR: cc.color(255, 135, 150),
+    },
+});
+
+module.exports = SB_Constants;
